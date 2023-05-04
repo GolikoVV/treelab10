326cf761988d832ea5649447d636392bbd608d79
Лера Голикова <funtik-98@mail.ru>
2023-05-04 09:49:23 +0700

    Добавьте файлы проекта.

---
+++ treelab10/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using treelab10;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Tree<int> tree = new Tree<int>();
+        tree.Add(4);
+        tree.Add(2);
+        tree.Add(1);
+        tree.Add(3);
+        tree.Add(6);
+        tree.Add(5);
+        tree.Add(7);
+
+        foreach (int value in tree)
+        {
+            Console.WriteLine(value);
+        }
+    }
+}
---
+++ treelab10/Tree.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace treelab10
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    public class Tree<T> : IEnumerable<T>
+    {
+        private Node<T> root;
+
+        public void Add(T data)
+        {
+            Node<T> newNode = new Node<T>(data);
+
+            if (root == null)
+            {
+                root = newNode;
+            }
+            else
+            {
+                AddNode(root, newNode);
+            }
+        }
+
+        private void AddNode(Node<T> currentNode, Node<T> newNode)
+        {
+            if (Comparer<T>.Default.Compare(newNode.Data, currentNode.Data) < 0)
+            {
+                if (currentNode.Left == null)
+                {
+                    currentNode.Left = newNode;
+                }
+                else
+                {
+                    AddNode(currentNode.Left, newNode);
+                }
+            }
+            else
+            {
+                if (currentNode.Right == null)
+                {
+                    currentNode.Right = newNode;
+                }
+                else
+                {
+                    AddNode(currentNode.Right, newNode);
+                }
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            Queue<Node<T>> queue = new Queue<Node<T>>();
+            queue.Enqueue(root);
+
+            while (queue.Count > 0)
+            {
+                Node<T> current = queue.Dequeue();
+                yield return current.Data;
+
+                if (current.Left != null)
+                {
+                    queue.Enqueue(current.Left);
+                }
+
+                if (current.Right != null)
+                {
+                    queue.Enqueue(current.Right);
+                }
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+
+    public class Node<T>
+    {
+        public T Data { get; set; }
+        public Node<T> Left { get; set; }
+        public Node<T> Right { get; set; }
+
+        public Node(T data)
+        {
+            Data = data;
+            Left = null;
+            Right = null;
+        }
+    }
+}
